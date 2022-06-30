0444b423412cae4fe5740f1afabf4d507cadc47d
GehaoYan <92266009+Steve724@users.noreply.github.com>
2022-06-29 22:35:58 -0400

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3030',
+            target: 'https://mysterious-wave-64325.herokuapp.com/',
             changeOrigin: true,
         })
     );
