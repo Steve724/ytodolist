07b15c49d43c75f7c38a9a57785cade3da003122
GehaoYan <92266009+Steve724@users.noreply.github.com>
2022-06-29 23:50:25 -0400

    add new url1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://mysterious-headland-08309.herokuapp.com',
+            target: 'http://localhost:3030',
             changeOrigin: true,
         })
     );
