249b9bd40a7d639d555e13af338663bbfe25de97
GehaoYan <92266009+Steve724@users.noreply.github.com>
2022-06-29 22:45:40 -0400

    change proxy2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default class Footer extends React.Component{
     render() {
         return(
             <footer>
-                <p>Join millions of people who organize work and life with Ytodolist.</p>
+                <h5>Join millions of people who organize work and life with Ytodolist.</h5>
                 <i className="fa-brands fa-twitter"></i>
                 <i className="fa-brands fa-youtube"></i>
                 <i className="fa-brands fa-facebook"></i>
