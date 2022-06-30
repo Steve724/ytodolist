850ef2d500226357dd7e3671f16269813b7e2226
GehaoYan <92266009+Steve724@users.noreply.github.com>
2022-06-29 22:42:56 -0400

    change proxy1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://mysterious-wave-64325.herokuapp.com/',
+            target: 'https://secret-fortress-65333.herokuapp.com',
             changeOrigin: true,
         })
     );
